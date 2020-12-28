4da8a48e4dfa23ab6fd72fa9667385bc5b433234
오한별 <suriessay@naver.com>
2020-12-28 23:51:14 +0900

    Initial commit

---
+++ src/test/java/com/just/anything/AnythingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.just.anything;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnythingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
