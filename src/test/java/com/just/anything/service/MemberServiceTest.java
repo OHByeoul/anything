bbe799adde88e8c9e60de52b769a3a571c86769d
오한별 <suriessay@naver.com>
2021-05-06 22:46:48 +0900

    회원관련 테스트 작성

---
+++ src/test/java/com/just/anything/service/MemberServiceTest.java
@@ -0,0 +1,50 @@
+package com.just.anything.service;
+
+import com.just.anything.domain.Member;
+import com.just.anything.repository.MemberRepository;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void 회원가입() throws Exception{
+        //given
+        Member member = new Member();
+        member.setName("nn1");
+        //when
+        Long savedId = memberService.join(member);
+        //then
+        assertEquals(member, memberRepository.findOne(savedId));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void 중복_회원_예외() throws Exception{
+        //given
+        Member member1 = new Member();
+        member1.setName("nn");
+
+        Member member2 = new Member();
+        member2.setName("nn");
+        //when
+        memberService.join(member1);
+        memberService.join(member2);
+
+        //then
+        Assert.fail("예외 발생해야 정상인 테스트");
+    }
+}
