1f4ac08e96a5602fe835d36cdd51c965c30620f6
오한별 <suriessay@naver.com>
2021-05-07 20:04:04 +0900

    상품 리포지토리 작성, 상품엔티티 추가 작성

--- src/main/java/com/just/anything/item/Item.java
+++ src/main/java/com/just/anything/item/Item.java
@@ -1,6 +1,7 @@
 package com.just.anything.item;
 
 import com.just.anything.domain.Category;
+import com.just.anything.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,7 +12,7 @@ import java.util.List;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE) //상속전략
 @DiscriminatorColumn(name="dtype")
-@Getter @Setter
+@Getter
 public abstract class Item {
     @Id
     @GeneratedValue
@@ -24,4 +25,19 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categorys = new ArrayList<>();
+    /*
+    * 재고 증가
+    * */
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity){
+        int result = this.stockQuantity - quantity;
+        if(result >= 0 ){
+            this.stockQuantity = result;
+        } else {
+            throw new NotEnoughStockException("need more stock");
+        }
+    }
 }
