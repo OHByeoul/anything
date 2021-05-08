8584f840d214f4927a621767b5365bfe1dba3d79
오한별 <suriessay@naver.com>
2021-05-08 19:26:27 +0900

    주문 엔티티에 주문생성,취소 등 추가

--- src/main/java/com/just/anything/domain/Order.java
+++ src/main/java/com/just/anything/domain/Order.java
@@ -46,4 +46,47 @@ public class Order {
         this.delivery = delivery;
         delivery.setOrder(this);
     }
+
+    /**
+     * 주문생성
+     */
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems){
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems){
+            order.addOrderItems(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    /**
+     * 주문취소
+     */
+    public void cancel(){
+        if(delivery.getStatus() == DeliveryStatus.COMP){
+            throw new IllegalStateException("이미 배송된 상품은 취소가 불가함");
+        }
+        this.setStatus(OrderStatus.CANCEL);
+        for(OrderItem orderItem : orderItems){
+            orderItem.cancel();
+        }
+    }
+
+    /*
+    * 전체 주문가격 조회
+    * */
+    public int getTotalPrice(){
+        int totalPrice = orderItems.stream()
+                .mapToInt(OrderItem::getTotalPrice)
+                .sum();
+        /*
+        for (OrderItem orderItem : orderItems){
+            totalPrice += orderItem.getTotalPrice();
+        }
+         */
+        return totalPrice;
+    }
 }
