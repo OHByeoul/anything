9dacad406f4b99788a16feb9eedc4932a6fcec02
오한별 <suriessay@naver.com>
2021-05-13 21:38:34 +0900

    홈화면 작성

--- src/main/java/com/just/anything/repository/OrderSearch.java
+++ src/main/java/com/just/anything/repository/OrderSearch.java
@@ -1,4 +1,11 @@
 package com.just.anything.repository;
 
+import com.just.anything.domain.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class OrderSearch {
+    private String memberName;
+    private OrderStatus orderStatus;
 }
