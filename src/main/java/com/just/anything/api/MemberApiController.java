81f41b4d6e4f06702f1d5495a31f6003568917a4
오한별 <suriessay@naver.com>
2021-05-26 22:04:39 +0900

    회원 업데이트 api 작성

--- src/main/java/com/just/anything/api/MemberApiController.java
+++ src/main/java/com/just/anything/api/MemberApiController.java
@@ -3,11 +3,11 @@ package com.just.anything.api;
 import com.just.anything.domain.Member;
 import com.just.anything.dto.RegistMeberRequest;
 import com.just.anything.dto.RegistMemberResponse;
+import com.just.anything.dto.UpdateMemberRequest;
+import com.just.anything.dto.UpdateMemberResponse;
 import com.just.anything.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -16,11 +16,18 @@ import javax.validation.Valid;
 public class MemberApiController {
     private final MemberService memberService;
 
-    @PostMapping("/member/v1/create")
+    @PostMapping("/api/v1/member")
     public RegistMemberResponse registMember(@RequestBody @Valid RegistMeberRequest request){
         Member member = new Member();
         member.setName(request.getName());
         Long id = memberService.join(member);
         return new RegistMemberResponse(id);
+    }
+
+    @PutMapping("/api/v1/members/{id}")
+    public UpdateMemberResponse updateMember(@PathVariable("id") Long id, @RequestBody @Valid UpdateMemberRequest request){
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(),findMember.getName());
     }
 }
---
+++ src/main/java/com/just/anything/dto/UpdateMemberRequest.java
@@ -0,0 +1,8 @@
+package com.just.anything.dto;
+
+import lombok.Data;
+
+@Data
+public class UpdateMemberRequest {
+    private String name;
+}
