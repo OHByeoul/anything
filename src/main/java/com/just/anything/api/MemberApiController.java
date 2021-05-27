16116172b5a041769bba989052dba9835f4a28a1
오한별 <suriessay@naver.com>
2021-05-27 22:22:02 +0900

    회원조회 api작성

--- src/main/java/com/just/anything/api/MemberApiController.java
+++ src/main/java/com/just/anything/api/MemberApiController.java
@@ -1,15 +1,14 @@
 package com.just.anything.api;
 
 import com.just.anything.domain.Member;
-import com.just.anything.dto.RegistMeberRequest;
-import com.just.anything.dto.RegistMemberResponse;
-import com.just.anything.dto.UpdateMemberRequest;
-import com.just.anything.dto.UpdateMemberResponse;
+import com.just.anything.dto.*;
 import com.just.anything.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,5 +28,15 @@ public class MemberApiController {
         memberService.update(id, request.getName());
         Member findMember = memberService.findOne(id);
         return new UpdateMemberResponse(findMember.getId(),findMember.getName());
+    }
+
+
+    @GetMapping("/api/v1/members")
+    public DataResult findMembers(){
+        List<Member> members = memberService.findMembers();
+        List<MemberDto> collect = members.stream().map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new DataResult(collect);
     }
 }
---
+++ src/main/java/com/just/anything/dto/MemberDto.java
@@ -0,0 +1,9 @@
+package com.just.anything.dto;
+
+import lombok.Data;
+
+@Data
+public class MemberDto {
+    public MemberDto(String name) {
+    }
+}
