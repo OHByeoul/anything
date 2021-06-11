44c610dcdafccb728cde6af8d2927713adbbc5e5
오한별 <suriessay@naver.com>
2021-06-12 00:08:25 +0900

    에러 수정

--- src/main/java/com/just/anything/repository/order/query/OrderQueryRepository2.java
+++ src/main/java/com/just/anything/repository/order/query/OrderQueryRepository2.java
@@ -12,25 +12,28 @@ import java.util.List;
 public class OrderQueryRepository2 {
     private final EntityManager em;
 
-    public List<OrderQueryDto> findOrderQueryDtos() {
-        List<OrderQueryDto> result = findOrders();
+    public List<OrderQueryDto2> findOrderQueryDtos() {
+        List<OrderQueryDto2> result = findOrders();
 
         result.forEach(o->{
             List<OrderItemQueryDto> orderItems = findOrderItems(o.getOrderId());
+            o.setOrderItems(orderItems);
         });
+        return result;
     }
 
     private List<OrderItemQueryDto> findOrderItems(Long orderId) {
-        return em.createQuery("select from OrderItem oi" +
+        return em.createQuery("select new com.just.anything.order.query.OrderItemQueryDto(oi.order.id,i.name,oi.orderPrice,oi.count)" +
+                " from OrderItem oi" +
                 " join oi.item i" +
                 " where oi.order.id = :orderId",OrderItemQueryDto.class)
                 .setParameter("orderId",orderId).getResultList();
     }
 
-    private List<OrderQueryDto> findOrders(){
-        return em.createQuery("select new com.just.anything.order.query.OrderQueryDto(o.id,m.name,o.orderDate,o.status,d.address) from Order o"+
+    private List<OrderQueryDto2> findOrders(){
+        return em.createQuery("select new com.just.anything.order.query.OrderQueryDto2(o.id,m.name,o.orderDate,o.status,d.address) from Order o"+
                 " join o.member m" +
-                "join o.delivery d",OrderQueryDto.class)
+                "join o.delivery d",OrderQueryDto2.class)
                 .getResultList();
     }
 }
